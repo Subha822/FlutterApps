10148fb1bbdecb122e796cdd22472128ac182729
Subha Rajagopal <73748056+Subha822@users.noreply.github.com>
2025-10-17 10:24:12 +0000

    quizzler

---
+++ quizzler/README.md
@@ -0,0 +1,16 @@
+# quizzler
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+
+## Screenshot
+
+![App Screenshot](images/screenshot.png)
+
+## Downlod APK
+
+📦 [Download APK](https://drive.google.com/file/d/15H8tJeIifSrN08bqup_3osjw54RPnNJN/view?usp=sharing)
---
+++ quizzler/lib/quiz_brain.dart
@@ -0,0 +1,74 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+
+  final List<Question> _questionBank = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was "Moon".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+      'No piece of square dry paper can be folded in half more than 7 times.',
+      false,
+    ),
+    Question(
+      'In London, UK, if you happen to die in the House of Parliament, '
+          'you are technically entitled to a state funeral, because the building '
+          'is considered too sacred a place.',
+      true,
+    ),
+    Question(
+      'The loudest sound produced by any animal is 188 decibels. '
+          'That animal is the African Elephant.',
+      false,
+    ),
+    Question(
+      'The total surface area of two human lungs is approximately 70 square metres.',
+      true,
+    ),
+    Question('Google was originally called "Backrub".', true),
+    Question(
+      'Chocolate affects a dog\'s heart and nervous system; a few ounces '
+          'are enough to kill a small dog.',
+      true,
+    ),
+    Question(
+      'In West Virginia, USA, if you accidentally hit an animal with your car, '
+          'you are free to take it home to eat.',
+      true,
+    ),
+  ];
+
+  /// Move to the next question, if available
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  /// Get current question text
+  String getQuestionText() => _questionBank[_questionNumber].questionText;
+
+  /// Get current correct answer
+  bool getCorrectAnswer() => _questionBank[_questionNumber].questionAnswer;
+
+  /// Check if quiz is finished
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('Quiz finished!');
+      return true;
+    }
+    return false;
+  }
+
+  /// Reset quiz back to the first question
+  void reset() {
+    _questionNumber = 0;
+  }
+
+  /// (Optional) Get total number of questions
+  int get totalQuestions => _questionBank.length;
+}
